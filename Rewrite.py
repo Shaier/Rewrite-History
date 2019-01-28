6d8a84ef2a2120636bc96f8c81b880f84b98919b
Sagi Shaier <Sagishaier@gmail.com>
2019-01-28 11:30:37 -0800

    Changing directories

--- Rewrite.py
+++ Rewrite.py
@@ -1,6 +1,6 @@
 #Change working directory to make it easier to access the files
 import os
-os.chdir("C:\Users\sagi\Documents\GitHub\Rewrite-History")
+os.chdir('C:\\Users\\sagi\\Documents\\GitHub\\Rewrite-History')
 os.getcwd()
 
 #Import libraries
