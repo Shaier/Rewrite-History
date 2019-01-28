5d620d96a6a873b18b8494830dae27eeb82dd684
Sagi Shaier <Sagishaier@gmail.com>
2019-01-28 10:43:02 -0800

    Changing directory

---
+++ Convert.py
@@ -0,0 +1,15 @@
+C:\Users\sagi\Documents\GitHub\Rewrite-History
+
+import PyPDF2
+import io
+import os
+
+for file in os.listdir('C:\\Users\\sagi\\Desktop\\Learning\\ML\\Datasets\\Rewrite History'): #Where the pdf is located
+    pdfFileObj = open('C:\\Users\\sagi\\Desktop\\Learning\\ML\\Datasets\\Rewrite History\\'+file,'rb')     #'rb' for read binary mode
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    pdfReader.numPages #number of pages in the file
+    with io.open('C:\\Users\\sagi\\Desktop\\Learning\\ML\\Datasets\\Rewrite History\\'+file+'.txt', 'w',encoding="utf-8") as f: #create a new txt file with the file's name
+        for i in range (1,pdfReader.numPages):
+            pageObj = pdfReader.getPage(i)          #'i' is the page number
+            line=pageObj.extractText()
+            f.write(line)
--- Rewrite.py
+++ Rewrite.py
@@ -1,1 +1,4 @@
-#C:\Users\sagi\Documents\GitHub\Rewrite-History
+#Change working directory to make it easier to access the files
+import os
+os.chdir("/content/gdrive/My Drive/Colab Notebooks/Rewrite")
+os.getcwd()
